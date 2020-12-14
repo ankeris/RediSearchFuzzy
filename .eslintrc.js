e9e3a6e0ac82fb30dc7b22c8c1dde63714411419
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-12-14 21:06:15 +0200

    fix(lint): settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,12 @@ module.exports = {
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
+        "@typescript-eslint/no-empty-interface": [
+            "error",
+            {
+                allowSingleExtends: true,
+            },
+        ],
     },
     settings: {
         react: {
