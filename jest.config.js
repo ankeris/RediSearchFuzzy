5d404f8987b151d9d1d1ec1f8dc828c27622ea55
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-12-16 22:22:29 +0200

    feat(tests): add addDocument tests

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,7 @@ module.exports = {
     testEnvironment: "node",
     moduleNameMapper: {
         "@enums/(.*)": ["<rootDir>/src/@core/enums/$1"],
+        "@utils/(.*)": ["<rootDir>/src/@core/utils/$1"],
     },
+    modulePathIgnorePatterns: ["<rootDir>/dist/"],
 };
---
+++ src/@features/index/__tests__/addDocument.test.ts
@@ -0,0 +1,27 @@
+import { addDocument } from "../indexList.service";
+import { generateAddDocumentCommandArgs } from "@utils/commands";
+import { RediSearchFuzzy } from "../../../index";
+import { RedisClient } from "redis";
+
+jest.mock("@utils/commands");
+jest.mock("../../../index");
+
+(generateAddDocumentCommandArgs as jest.Mock).mockImplementation(() => ({ args: [] }));
+(RediSearchFuzzy as jest.Mock).mockImplementation(() => ({
+    client: {
+        hset: () => {},
+    },
+}));
+
+describe("generateSchemaIndexCommand()", () => {
+    it("generateSchemaIndexCommand should call generateAddDocumentCommandArgs and pass the document", () => {
+        addDocument({
+            context: new RediSearchFuzzy({} as RedisClient),
+            key: "products",
+            document: {
+                testKey: "testValue",
+            },
+        });
+        expect(generateAddDocumentCommandArgs).toHaveBeenCalledWith({ document: { testKey: "testValue" } });
+    });
+});
